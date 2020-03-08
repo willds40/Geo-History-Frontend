9a6fa68531427d7f02be4f7b46091c9117ed28b2
Your Name <you@example.com>
2020-03-08 12:47:33 -0700

    feat: add button

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders should find button that reads click me', () => {
+  render(<App />);
+  expect(document.querySelector("button")).toHaveTextContent("Click Me")
 });
+
+
