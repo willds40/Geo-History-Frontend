b918ee528f89fb3f78ab6d9e7e4d2d3726023cd7
willds40@gmail.com <Wsand@gap.com>
2020-03-11 15:53:24 -0700

    feat: add user info text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const handleClick = () =>{
+   //fetch data from backend 
+   console.log("here")
+    updateText("User Info")
+  }
+
+  const [text, updateText] = useState("")
+
   return (
     <div className="App">
-      <button>Click Me</button>
+      <button onClick={handleClick}>Click Me</button>
+      <div data-testid="user-info">{text}</div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,21 @@ import { render } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 import App from './App';
 
-test('renders should find button that reads click me', () => {
-  render(<App />);
-  expect(document.querySelector("button")).toHaveTextContent("Click Me")
+test('renders button that reads click me', () => {
+  const {queryByText} = render(<App />);
+  expect(queryByText("Click Me")).not.toBeNull();
+});
+
+test('renders empty div on load', () => {
+  const {getByTestId} = render(<App />);
+  expect(getByTestId("user-info")).toHaveTextContent("")
+});
+
+test('renders user info text when button is clicked', () => {
+  const {queryByText,getByTestId} = render(<App />);
+  const button = queryByText("Click Me")
+  button.click()
+  expect(getByTestId("user-info")).toHaveTextContent("User Info");
 });
 
 
